a10576951060f79b63c0a7909b423ae263c60911
memothelemo <dev@memothelemo.xyz>
2026-09-08 23:27:29 +0800

    Activity done

--- src/frames_extractor/__main__.py
+++ src/frames_extractor/__main__.py
@@ -1,5 +1,9 @@
 import cv2
+import os
+import pathlib
 import sys
+
+from pathlib import Path
 
 # Opens the captured video from the specified path.
 capture = cv2.VideoCapture("input.mp4")
@@ -8,35 +12,30 @@ if not capture.isOpened():
     print("[x] Failed to load input.mp4 file!")
     sys.exit(1)
 
-print("[!] Generating mirrored video from input.mp4...")
+print("[!] Extracting frames from input.mp4...")
 
 # We need to get the captured device's width, height and its FPS for video encoding soon.
 cap_frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 cap_frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 cap_frame_fps = int(capture.get(cv2.CAP_PROP_FPS))
 
-# Loads the MP4V codec
-mp4v_codec = cv2.VideoWriter.fourcc(*"mp4v")
+# Create output folder for frames
+output_folder = Path(__file__).parent.parent.parent / "extracted_frames"
+os.makedirs(output_folder, exist_ok=True)
 
-# Writer file stream for the recorded video
-writer = cv2.VideoWriter(
-    filename="output.mp4",
-    fourcc=mp4v_codec,
-    fps=cap_frame_fps,
-    frameSize=(cap_frame_width, cap_frame_height)
-)
+# Extract every frame from the video
+extracted_frames = 0
 
-while capture.isOpened():
+while True:
     ret, frame = capture.read()
 
     # The video capture stream is exhausted if it returns false.
     if not ret: break
 
-    # 1 means horizontal flip
-    flipped_frame = cv2.flip(frame, 1)
-    writer.write(flipped_frame)
+    frame_file_name = output_folder / f"frame_{extracted_frames:04d}.png"
+    cv2.imwrite(frame_file_name, frame)
 
-writer.release()
-print(f"[/] Saved mirror video at output.mp4")
+    extracted_frames += 1
 
 capture.release()
+print(f"[/] Successfully extracted {extracted_frames} frame(s).")
